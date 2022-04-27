0936e09db29824a978a94dfe7b92b1634155a620
karlaferry <karlacarreon.1994@gmail.com>
2022-04-27 15:42:17 +0100

    remove console log

--- src/components/SingleDailyCelebrant.jsx
+++ src/components/SingleDailyCelebrant.jsx
@@ -2,16 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function SingleDailyCelebrant({ dailyCeleb }) {
-  console.log(dailyCeleb);
-  // const messageHandler = () => {
-  //   console.log("Clicked!!!");
-  // };
-  return (
-    <div>
-      <img src={`${dailyCeleb.avatar_url}`} alt="celebrant avatar" />
-      <h3>
-        <Link to={`/celebrant/${dailyCeleb.id}`}>{dailyCeleb.first_name}</Link>
-      </h3>
-    </div>
-  );
+	return (
+		<div>
+			<img src={`${dailyCeleb.avatar_url}`} alt="celebrant avatar" />
+			<h3>
+				<Link to={`/celebrant/${dailyCeleb.id}`}>{dailyCeleb.first_name}</Link>
+			</h3>
+		</div>
+	);
 }
