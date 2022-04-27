23d50f4b760c6ae1e37ef09c00f63c4830c09a6d
karlaferry <karlacarreon.1994@gmail.com>
2022-04-27 15:53:33 +0100

    create Home component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
-import Monthly from "./components/Monthly";
-import Daily from "./components/Daily";
+import Home from "./components/Home";
 import CelebrantCard from "./components/CelebrantCard";
 import { getUsers } from "./utils/dbCalls";
 
 function App() {
-  getUsers();
-  return (
-    <div>
-      <BrowserRouter>
-        <Header />
-        <Monthly />
-        <Daily />
-        <Routes>
-          <Route path="/celebrant/:id" element={<CelebrantCard />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+	getUsers();
+	return (
+		<div>
+			<BrowserRouter>
+				<Header />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/celebrant/:id" element={<CelebrantCard />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Monthly from "./Monthly";
+import Daily from "./Daily";
+
+export default function Home() {
+	return (
+		<div>
+			<Monthly />
+			<Daily />
+		</div>
+	);
+}
