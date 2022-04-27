3c6c8c02fcbf52881afdf172169afd847871fb99
karlaferry <karlacarreon.1994@gmail.com>
2022-04-27 15:42:11 +0100

    add monthly celebrant avatars

--- src/components/SingleMonthlyCelebrant.jsx
+++ src/components/SingleMonthlyCelebrant.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import defaultAvatar from "../defaultAvatar.png";
 
 export default function SingleMonthlyCelebrant({ monthlyCeleb }) {
-  return (
-    <div>
-      <h3>{monthlyCeleb.birth_date.split("/")[0]}</h3>
-      <img src={defaultAvatar} width="100" />
-      <p>{monthlyCeleb.first_name}</p>
-    </div>
-  );
+	return (
+		<div>
+			<h3>{monthlyCeleb.birth_date.split("/")[0]}</h3>
+			<img src={`${monthlyCeleb.avatar_url}`} alt="monthly celebrant avatar" />
+			<p>{monthlyCeleb.first_name}</p>
+		</div>
+	);
 }
