495774ac46aec55c667a09b211784643d1d85006
karlaferry <karlacarreon.1994@gmail.com>
2022-04-28 12:38:11 +0100

    initial setup

--- src/components/CelebrantCard.jsx
+++ src/components/CelebrantCard.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-
+import { Link, useParams } from "react-router-dom";
 export default function CelebrantCard() {
-  return <h2>CelebrantCard</h2>;
+	const { id } = useParams();
+	return (
+		<div>
+			<h2>CelebrantCard</h2>
+			<Link to="/">Back to Home</Link>
+		</div>
+	);
 }
