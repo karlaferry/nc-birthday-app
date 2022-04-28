87a5a9f25925eacdda62890698340afda12784b0
karlaferry <karlacarreon.1994@gmail.com>
2022-04-28 12:37:46 +0100

    wrap whole div in Link

--- src/components/SingleDailyCelebrant.jsx
+++ src/components/SingleDailyCelebrant.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 export default function SingleDailyCelebrant({ dailyCeleb }) {
 	return (
-		<div>
-			<img src={`${dailyCeleb.avatar_url}`} alt="celebrant avatar" />
-			<h3>
-				<Link to={`/celebrant/${dailyCeleb.id}`}>{dailyCeleb.first_name}</Link>
-			</h3>
-		</div>
+		<Link to={`/celebrant/${dailyCeleb.id}`}>
+			<div>
+				<img src={`${dailyCeleb.avatar_url}`} alt="celebrant avatar" />
+				<h3>{dailyCeleb.first_name}</h3>
+			</div>
+		</Link>
 	);
 }
