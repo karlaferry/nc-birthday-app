a17bbc633da93077da82281f3a3660c74f2a2e2b
karlaferry <karlacarreon.1994@gmail.com>
2022-04-28 16:54:56 +0100

    implement login error handling

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,49 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase/firebaseConfig";
+import { UserContext } from "../Contexts/User";
+import { extractErrorMsg } from "../utils/helperFuncs";
 
 export default function Login() {
+	const [logInEmail, setLogInEmail] = useState("");
+	const [logInPassword, setLogInPassword] = useState("");
+	const [errorMsg, setErrorMsg] = useState("");
+	const { user } = useContext(UserContext);
+	const navigate = useNavigate();
+
+	const handleEmail = (e) => {
+		setLogInEmail(e.target.value);
+	};
+	const handlePassword = (e) => {
+		setLogInPassword(e.target.value);
+	};
+
+	const logIn = async (e) => {
+		try {
+			e.preventDefault();
+			await signInWithEmailAndPassword(auth, logInEmail, logInPassword);
+			navigate("/dashboard");
+		} catch (e) {
+			setErrorMsg(extractErrorMsg(e.code));
+			console.log(e);
+		}
+	};
+
 	return (
 		<div>
 			<h1>Login</h1>
 			<form>
-				<input type="email" placeholder="E-mail" />
-				<input type="password" placeholder="Password" />
-				<button>Login</button>
+				<input type="email" placeholder="E-mail" onChange={handleEmail} />
+				<input
+					type="password"
+					placeholder="Password"
+					onChange={handlePassword}
+				/>
+				<button onClick={logIn}>Sign Up</button>
 			</form>
+			{user && <h2>{user.email}</h2>}
+			{errorMsg && <h3>{errorMsg}</h3>}
 		</div>
 	);
 }
--- src/utils/helperFuncs.js
+++ src/utils/helperFuncs.js
@@ -1,42 +1,48 @@
 exports.getMonthAndYear = () => {
-  const date = new Date();
-  const month = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  return `${month[date.getMonth()]} ${date.getFullYear()}`;
+	const date = new Date();
+	const month = [
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December",
+	];
+	return `${month[date.getMonth()]} ${date.getFullYear()}`;
 };
 
 exports.getMonthNum = () => {
-  const date = new Date();
-  return date.getMonth() + 1;
+	const date = new Date();
+	return date.getMonth() + 1;
 };
 
 exports.getDay = () => {
-  const date = new Date();
-  return date.getDate();
+	const date = new Date();
+	return date.getDate();
 };
 
 exports.sortBirthdays = (array) => {
-  return array.sort((a, b) => {
-    const previousBirthday = +a.birth_date.split("/")[0];
-    const currentBirthday = +b.birth_date.split("/")[0];
-    let comparison = 0;
-    if (previousBirthday > currentBirthday) {
-      comparison = 1;
-    } else if (previousBirthday < currentBirthday) {
-      comparison = -1;
-    }
-    return comparison;
-  });
+	return array.sort((a, b) => {
+		const previousBirthday = +a.birth_date.split("/")[0];
+		const currentBirthday = +b.birth_date.split("/")[0];
+		let comparison = 0;
+		if (previousBirthday > currentBirthday) {
+			comparison = 1;
+		} else if (previousBirthday < currentBirthday) {
+			comparison = -1;
+		}
+		return comparison;
+	});
+};
+
+exports.extractErrorMsg = (str) => {
+	const splitMsg = str.split("/")[1].split("-");
+	const errorMsg = splitMsg.join(" ").toUpperCase() + ".";
+	return errorMsg;
 };
