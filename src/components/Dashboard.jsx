217c9260eb2ec1a61e429c80378ebe78446f38fe
karlaferry <karlacarreon.1994@gmail.com>
2022-05-03 16:41:13 +0100

    implement avatar upload

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,27 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { storage } from "../firebase";
+import { ref, uploadBytes } from "firebase/storage";
 import { UserContext } from "../Contexts/User";
 
 export default function Dashboard() {
 	const { user, isLoggedIn, userData } = useContext(UserContext);
+	const [avatar, setAvatar] = useState(null);
 	const navigate = useNavigate();
+
+	const handleFile = (e) => {
+		const image = e.target.files[0];
+		image && setAvatar(image);
+	};
+
+	const handleUpload = async (e) => {
+		e.preventDefault();
+		if (avatar === null) return;
+		const imageRef = ref(storage, `user_avatars/${user.uid}`);
+		await uploadBytes(imageRef, avatar);
+		alert("Image successfully uploaded.");
+		window.location.reload();
+	};
 
 	return (
 		<div>
@@ -16,14 +33,9 @@ export default function Dashboard() {
 					<img src={`${userData.avatar_url}`} alt="user default" />
 					<p>Name: {userData.first_name}</p>
 					<p>Email: {user.email}</p>
-					<form
-						onSubmit={(e) => {
-							e.preventDefault();
-							console.log(e.target.value);
-						}}
-					>
+					<form onSubmit={handleUpload}>
 						<label>Change Avatar:</label>
-						<input type="file" accept="image/*" />
+						<input type="file" accept="image/*" onChange={handleFile} />
 						<button>Upload</button>
 					</form>
 					<br />
