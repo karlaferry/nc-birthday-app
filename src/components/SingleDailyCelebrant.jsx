a9156519842ccf191e7846a5a678e07dd0470506
karlaferry <karlacarreon.1994@gmail.com>
2022-05-04 23:19:55 +0100

    remove console log

--- src/components/SingleDailyCelebrant.jsx
+++ src/components/SingleDailyCelebrant.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function SingleDailyCelebrant({ dailyCeleb }) {
-  console.log(dailyCeleb.birth_date);
   return (
     <Link to={`/celebrant/${dailyCeleb.id}`}>
       <div>
