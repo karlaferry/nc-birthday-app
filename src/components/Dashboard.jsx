4f4d70d832c3060c8d4859e7bce7b187049242e1
karlaferry <karlacarreon.1994@gmail.com>
2022-05-04 17:37:17 +0100

    implement feature to upload new user avatar

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,49 +3,51 @@ import { useNavigate } from "react-router-dom";
 import { storage } from "../firebase";
 import { ref, uploadBytes } from "firebase/storage";
 import { UserContext } from "../Contexts/User";
+import { patchUserAvatar } from "../utils/dbCalls";
 
 export default function Dashboard() {
-	const { user, isLoggedIn, userData } = useContext(UserContext);
-	const [avatar, setAvatar] = useState(null);
-	const navigate = useNavigate();
+  const { user, isLoggedIn, userData } = useContext(UserContext);
+  const [avatar, setAvatar] = useState(null);
+  const navigate = useNavigate();
 
-	const handleFile = (e) => {
-		const image = e.target.files[0];
-		image && setAvatar(image);
-	};
+  const handleFile = (e) => {
+    const image = e.target.files[0];
+    image && setAvatar(image);
+  };
 
-	const handleUpload = async (e) => {
-		e.preventDefault();
-		if (avatar === null) return;
-		const imageRef = ref(storage, `user_avatars/${user.uid}`);
-		await uploadBytes(imageRef, avatar);
-		alert("Image successfully uploaded.");
-		window.location.reload();
-	};
+  const handleUpload = async (e) => {
+    e.preventDefault();
+    if (avatar === null) return;
+    const imageRef = ref(storage, `user_avatars/${user.uid}`);
+    await uploadBytes(imageRef, avatar);
+    await patchUserAvatar(user.uid);
+    alert("Image successfully uploaded.");
+    window.location.reload();
+  };
 
-	return (
-		<div>
-			{isLoggedIn === true ? (
-				<>
-					<h2>Dashboard</h2>
-					<h3>Welcome, {userData.first_name}!</h3>
-					<h2>Account Info</h2>
-					<img src={`${userData.avatar_url}`} alt="user default" />
-					<p>Name: {userData.first_name}</p>
-					<p>Email: {user.email}</p>
-					<form onSubmit={handleUpload}>
-						<label>Change Avatar:</label>
-						<input type="file" accept="image/*" onChange={handleFile} />
-						<button>Upload</button>
-					</form>
-					<br />
-					<button onClick={() => navigate(`/celebrant/${user.uid}`)}>
-						My Birthday Page
-					</button>
-				</>
-			) : (
-				<p>Please login or register.</p>
-			)}
-		</div>
-	);
+  return (
+    <div>
+      {isLoggedIn === true ? (
+        <>
+          <h2>Dashboard</h2>
+          <h3>Welcome, {userData.first_name}!</h3>
+          <h2>Account Info</h2>
+          <img src={`${userData.avatar_url}`} alt="user default" />
+          <p>Name: {userData.first_name}</p>
+          <p>Email: {user.email}</p>
+          <form onSubmit={handleUpload}>
+            <label>Change Avatar:</label>
+            <input type="file" accept="image/*" onChange={handleFile} />
+            <button>Upload</button>
+          </form>
+          <br />
+          <button onClick={() => navigate(`/celebrant/${user.uid}`)}>
+            My Birthday Page
+          </button>
+        </>
+      ) : (
+        <p>Please login or register.</p>
+      )}
+    </div>
+  );
 }
--- src/utils/dbCalls.js
+++ src/utils/dbCalls.js
@@ -1,50 +1,61 @@
-import { database } from "../firebase";
-import { ref, child, get, set } from "firebase/database";
+import { database, storage } from "../firebase";
+import { ref, child, get, set, update } from "firebase/database";
 import { getMonthNum, getDay } from "./helperFuncs";
+import { getDownloadURL, ref as sRef } from "firebase/storage";
 
 export const getUsers = () => {
-	const usersDb = ref(database);
-	return get(child(usersDb, `/users`)).then((snapshot) => {
-		return snapshot.val();
-	});
+  const usersDb = ref(database);
+  return get(child(usersDb, `/users`)).then((snapshot) => {
+    return snapshot.val();
+  });
 };
 
 export const getSingleUser = (id) => {
-	const usersDb = ref(database);
-	return get(child(usersDb, `/users/` + id)).then((snapshot) => {
-		return snapshot.val();
-	});
+  const usersDb = ref(database);
+  return get(child(usersDb, `/users/` + id)).then((snapshot) => {
+    return snapshot.val();
+  });
 };
 
 export const postUser = (userId, firstName, birthdate) => {
-	const postUserDb = ref(database, "/users/" + userId);
-	const newUser = {
-		avatar_url:
-			"https://firebasestorage.googleapis.com/v0/b/nc-birthday-app.appspot.com/o/user.png?alt=media&token=b6322e5d-7b78-4caf-884d-5aa63712a558",
-		first_name: firstName,
-		birth_date: birthdate,
-		id: userId,
-	};
-	set(postUserDb, newUser);
+  const postUserDb = ref(database, "/users/" + userId);
+  const newUser = {
+    avatar_url:
+      "https://firebasestorage.googleapis.com/v0/b/nc-birthday-app.appspot.com/o/user.png?alt=media&token=b6322e5d-7b78-4caf-884d-5aa63712a558",
+    first_name: firstName,
+    birth_date: birthdate,
+    id: userId,
+  };
+  set(postUserDb, newUser);
 };
 
 // ignore these next two calls, haven't figured out how to query!
 export const getMonthlyCelebrants = async () => {
-	const users = await getUsers();
-	const monthlyCelebs = [];
-	for (let user in users) {
-		const userMonth = +users[user]["birth_date"].split("/")[1];
-		userMonth === getMonthNum() && monthlyCelebs.push(users[user]);
-	}
-	return monthlyCelebs;
+  const users = await getUsers();
+  const monthlyCelebs = [];
+  for (let user in users) {
+    const userMonth = +users[user]["birth_date"].split("/")[1];
+    userMonth === getMonthNum() && monthlyCelebs.push(users[user]);
+  }
+  return monthlyCelebs;
 };
 
 export const getDailyCelebrants = async () => {
-	const users = await getUsers();
-	const dailyCelebs = [];
-	for (let user in users) {
-		users[user].birth_date.startsWith(`${getDay()}/${getMonthNum()}`) &&
-			dailyCelebs.push(users[user]);
-	}
-	return dailyCelebs;
+  const users = await getUsers();
+  const dailyCelebs = [];
+  for (let user in users) {
+    users[user].birth_date.startsWith(`${getDay()}/${getMonthNum()}`) &&
+      dailyCelebs.push(users[user]);
+  }
+  return dailyCelebs;
+};
+
+export const patchUserAvatar = async (userId) => {
+  const avatarRef = sRef(storage, `user_avatars/${userId}`);
+  const url = await getDownloadURL(avatarRef);
+  const dbRef = ref(database);
+  const usersRef = child(dbRef, `/users/` + userId);
+  await update(usersRef, {
+    avatar_url: url,
+  });
 };
