a6a3d975eae54a8385f46c85d86574166d8eea63
karlaferry <karlacarreon.1994@gmail.com>
2022-05-04 21:41:18 +0100

    add try/catch

--- src/utils/dbCalls.js
+++ src/utils/dbCalls.js
@@ -1,67 +1,97 @@
 import { database, storage, auth } from "../firebase";
 import { deleteUser } from "firebase/auth";
 import { ref, child, get, set, update } from "firebase/database";
-import { getDownloadURL, ref as sRef } from "firebase/storage";
+import { getDownloadURL, ref as sRef, deleteObject } from "firebase/storage";
 import { getMonthNum, getDay } from "./helperFuncs";
 
 export const getUsers = async () => {
-  const usersDb = ref(database);
-  const snapshot = await get(child(usersDb, `/users`));
-  return snapshot.val();
+  try {
+    const usersDb = ref(database);
+    const snapshot = await get(child(usersDb, `/users`));
+    return snapshot.val();
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 export const getSingleUser = async (userId) => {
-  const usersDb = ref(database);
-  const snapshot = await get(child(usersDb, `/users/` + userId));
-  return snapshot.val();
+  try {
+    const usersDb = ref(database);
+    const snapshot = await get(child(usersDb, `/users/` + userId));
+    return snapshot.val();
+  } catch (e) {
+    console.log(e);
+  }
 };
 
-export const postUser = (userId, firstName, birthdate) => {
-  const postUserDb = ref(database, "/users/" + userId);
-  const newUser = {
-    avatar_url:
-      "https://firebasestorage.googleapis.com/v0/b/nc-birthday-app.appspot.com/o/user.png?alt=media&token=b6322e5d-7b78-4caf-884d-5aa63712a558",
-    first_name: firstName,
-    birth_date: birthdate,
-    id: userId,
-  };
-  set(postUserDb, newUser);
+export const postUser = async (userId, firstName, birthdate) => {
+  try {
+    const postUserDb = ref(database, "/users/" + userId);
+    const newUser = {
+      avatar_url:
+        "https://firebasestorage.googleapis.com/v0/b/nc-birthday-app.appspot.com/o/user.png?alt=media&token=b6322e5d-7b78-4caf-884d-5aa63712a558",
+      first_name: firstName,
+      birth_date: birthdate,
+      id: userId,
+    };
+    await set(postUserDb, newUser);
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 // ignore these next two calls, haven't figured out how to query!
 export const getMonthlyCelebrants = async () => {
-  const users = await getUsers();
-  const monthlyCelebs = [];
-  for (let user in users) {
-    const userMonth = +users[user]["birth_date"].split("/")[1];
-    userMonth === getMonthNum() && monthlyCelebs.push(users[user]);
+  try {
+    const users = await getUsers();
+    const monthlyCelebs = [];
+    for (let user in users) {
+      const userMonth = +users[user]["birth_date"].split("/")[1];
+      userMonth === getMonthNum() && monthlyCelebs.push(users[user]);
+    }
+    return monthlyCelebs;
+  } catch (e) {
+    console.log(e);
   }
-  return monthlyCelebs;
 };
 
 export const getDailyCelebrants = async () => {
-  const users = await getUsers();
-  const dailyCelebs = [];
-  for (let user in users) {
-    users[user].birth_date.startsWith(`${getDay()}/${getMonthNum()}`) &&
-      dailyCelebs.push(users[user]);
+  try {
+    const users = await getUsers();
+    const dailyCelebs = [];
+    for (let user in users) {
+      users[user].birth_date.startsWith(`${getDay()}/${getMonthNum()}`) &&
+        dailyCelebs.push(users[user]);
+    }
+    return dailyCelebs;
+  } catch (e) {
+    console.log(e);
   }
-  return dailyCelebs;
 };
 
 export const patchUserAvatar = async (userId) => {
-  const avatarRef = sRef(storage, `user_avatars/${userId}`);
-  const url = await getDownloadURL(avatarRef);
-  const dbRef = ref(database);
-  const usersRef = child(dbRef, `/users/` + userId);
-  await update(usersRef, {
-    avatar_url: url,
-  });
+  try {
+    const avatarRef = sRef(storage, `user_avatars/${userId}`);
+    const url = await getDownloadURL(avatarRef);
+    const dbRef = ref(database);
+    const usersRef = child(dbRef, `/users/` + userId);
+    await update(usersRef, {
+      avatar_url: url,
+    });
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 export const deleteAccount = async (userId) => {
-  const dbRef = ref(database);
-  const usersRef = child(dbRef, `/users/` + userId);
-  await set(usersRef, null);
-  await deleteUser(auth.currentUser);
+  try {
+    const dbRef = ref(database);
+    const usersRef = child(dbRef, `/users/` + userId);
+    const avatarRef = sRef(storage, `user_avatars/${userId}`);
+    await set(usersRef, null);
+    await deleteUser(auth.currentUser);
+    await deleteObject(avatarRef);
+  } catch (e) {
+    console.log(e);
+  }
 };
