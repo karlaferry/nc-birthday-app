f34420b2903e8416c5a866bc9c7a9415e406b0eb
karlaferry <karlacarreon.1994@gmail.com>
2022-05-06 08:47:44 +0100

    add missing useEffect dependency

--- src/components/CelebrantCard.jsx
+++ src/components/CelebrantCard.jsx
@@ -5,45 +5,45 @@ import MessageField from "./MessageField";
 import { getSingleUser, getGreetings } from "../utils/dbCalls";
 
 export default function CelebrantCard() {
-  const { id } = useParams();
-  const { isLoggedIn } = useContext(UserContext);
-  const [currentCeleb, setCurrentCeleb] = useState({});
-  const [celebGreetings, setCelebGreetings] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+	const { id } = useParams();
+	const { isLoggedIn } = useContext(UserContext);
+	const [currentCeleb, setCurrentCeleb] = useState({});
+	const [celebGreetings, setCelebGreetings] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    setIsLoading(true);
-    async function loadPage() {
-      const singleCeleb = await getSingleUser(id);
-      const greetings = await getGreetings(id);
-      setCelebGreetings((curr) => {
-        const arr = [];
-        for (let greeting in greetings) {
-          arr.push(greetings[greeting]);
-        }
-        return arr;
-      });
-      setCurrentCeleb(singleCeleb);
-    }
-    loadPage();
-    setIsLoading(false);
-  }, []);
+	useEffect(() => {
+		setIsLoading(true);
+		async function loadPage() {
+			const singleCeleb = await getSingleUser(id);
+			const greetings = await getGreetings(id);
+			setCelebGreetings((curr) => {
+				const arr = [];
+				for (let greeting in greetings) {
+					arr.push(greetings[greeting]);
+				}
+				return arr;
+			});
+			setCurrentCeleb(singleCeleb);
+		}
+		loadPage();
+		setIsLoading(false);
+	}, [id]);
 
-  return (
-    <div>
-      <h1>{currentCeleb.first_name}</h1>
-      <img
-        src={`${currentCeleb.avatar_url}`}
-        alt="daily celebrant"
-        width="20%"
-      />
-      <MessageField id={id} />
-      {/* Make greetings component here */}
-      {/* Add isLoading conditional rendering */}
-      {isLoggedIn &&
-        celebGreetings.map((greeting) => (
-          <h3 key={`${greeting.timestamp}`}>{greeting.message}</h3>
-        ))}
-    </div>
-  );
+	return (
+		<div>
+			<h1>{currentCeleb.first_name}</h1>
+			<img
+				src={`${currentCeleb.avatar_url}`}
+				alt="daily celebrant"
+				width="20%"
+			/>
+			<MessageField id={id} />
+			{/* Make greetings component here */}
+			{/* Add isLoading conditional rendering */}
+			{isLoggedIn &&
+				celebGreetings.map((greeting) => (
+					<h3 key={`${greeting.timestamp}`}>{greeting.message}</h3>
+				))}
+		</div>
+	);
 }
