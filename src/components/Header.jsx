44319b4f3d337ceacb95141373a83fcc17913d10
karlaferry <karlacarreon.1994@gmail.com>
2022-05-10 15:04:03 +0100

    change app name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import NavBar from "./NavBar";
 
 export default function Header() {
-	return (
-		<div>
-			<h1>NC-Birthday-App</h1>
-			<NavBar />
-		</div>
-	);
+  return (
+    <div>
+      <h1>NC Birthdays</h1>
+      <NavBar />
+    </div>
+  );
 }
