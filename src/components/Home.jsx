5d3a30b48dc9106a1c678b1428cd56c94c9aa12d
karlaferry <karlacarreon.1994@gmail.com>
2022-05-11 15:28:54 +0100

    cleanup

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,8 +4,6 @@ import Daily from "./Daily";
 import { UserContext } from "../Contexts/User";
 
 export default function Home() {
-  const { isLoggedIn } = useContext(UserContext);
-  console.log(isLoggedIn);
   return (
     <div>
       <Monthly />
