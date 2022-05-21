2d3e6d5bcef0ed6247c855dc8dcddbe9fe527edb
karlaferry <karlacarreon.1994@gmail.com>
2022-05-21 14:10:48 +0100

    tailwind setup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import NavBar from "./NavBar";
 export default function Header() {
   return (
     <div>
-      <h1>NC Birthdays</h1>
+      <h1 className="text-3xl font-bold underline">NC Birthdays</h1>
       <NavBar />
     </div>
   );
