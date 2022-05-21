7792c2479363fb63349a62eb4dab583de9b54473
karlaferry <karlacarreon.1994@gmail.com>
2022-05-21 19:59:13 +0100

    initial styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,27 +6,43 @@ import { UserContext } from "../Contexts/User";
 
 export default function NavBar() {
   const { isLoggedIn } = useContext(UserContext);
+  const buttons =
+    "bg-accent1 hover:bg-accent2 hover:text-primary2 font-varela font-bold text-accent2 text-lg py-3 px-16 min-w-full hover:border-blue-500 rounded-md lowercase m-1";
   return (
-    <div>
+    <div className="flex flex-col items-center content-center">
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/">
+            <button className={buttons}>Home</button>
+          </Link>
         </li>
         {!isLoggedIn && (
           <li>
-            <Link to="/register-login">Register | Login</Link>
+            <Link to="/register-login">
+              <button className={buttons}>Register | Login</button>
+            </Link>
           </li>
         )}
         {isLoggedIn && (
           <li>
-            <Link to="/dashboard">Dashboard</Link>
+            <Link to="/dashboard">
+              <button className={buttons}>Dashboard</button>
+            </Link>
           </li>
         )}
         {isLoggedIn && (
           <li>
-            <a onClick={async () => await signOut(auth)} href="/">
-              Logout
-            </a>
+            <form action="/">
+              <button
+                onClick={async () => {
+                  console.log("clicked!");
+                  await signOut(auth);
+                }}
+                className={buttons}
+              >
+                Logout
+              </button>
+            </form>
           </li>
         )}
       </ul>
