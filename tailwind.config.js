ef68e474673e5a6af8c738e7f170419551cd098d
karlaferry <karlacarreon.1994@gmail.com>
2022-05-21 14:29:06 +0100

    set theme colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      primary1: "#E63946",
+      primary2: "#F1FAEE",
+      primary3: "#1D3557",
+      accent1: "#A8DADC",
+      accent2: "#457B9D",
+    },
   },
   plugins: [],
 };
