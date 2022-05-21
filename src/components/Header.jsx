b4dea3470989565d7eebb075533002fc3f37eb04
karlaferry <karlacarreon.1994@gmail.com>
2022-05-21 14:29:06 +0100

    set theme colours

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,9 @@ import NavBar from "./NavBar";
 export default function Header() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">NC Birthdays</h1>
+      <h1 className="text-3xl font-bold text-center m-6 uppercase text-primary1">
+        🎂 NC Birthdays
+      </h1>
       <NavBar />
     </div>
   );
