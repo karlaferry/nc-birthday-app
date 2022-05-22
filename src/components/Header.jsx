1c0d0d8ee5541ac34f291e8499570b7cf2367d97
karlaferry <karlacarreon.1994@gmail.com>
2022-05-22 15:44:10 +0100

    initial layout for mobile and laptop + remove tablet screen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,33 @@ import React from "react";
 import NavBar from "./NavBar";
 
 export default function Header() {
+  const headerContainer = `
+  flex 
+  mobile:flex-col 
+  laptop:flex-row
+  flex-grow
+  justify-evenly
+  `;
+
+  const headerStyle = `
+  mobile:text-4xl 
+  laptop:text-5xl
+  font-bold 
+  text-center 
+  mobile:px-4 
+  pb-6 
+  uppercase 
+  text-primary1 
+  font-varela
+  w-full
+  mobile:pt-10 
+  laptop:pl-14
+  laptop:pt-14 
+  laptop:text-left
+  `;
   return (
-    <div>
-      <h1 className="text-4xl font-bold text-center px-4 pt-10 pb-6 uppercase text-primary1 font-varela">
-        🎂 NC Birthdays
-      </h1>
+    <div className={headerContainer}>
+      <h1 className={headerStyle}>🎂 NC Birthdays</h1>
       <NavBar />
     </div>
   );
