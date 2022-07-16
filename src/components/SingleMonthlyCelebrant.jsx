3cbb9f56d36a2d830e42e71148ce21dd32dd5b1d
karlaferry <karlacarreon.1994@gmail.com>
2022-07-16 16:29:01 +0100

    set names of monthly celebrants to uppercase

--- src/components/SingleMonthlyCelebrant.jsx
+++ src/components/SingleMonthlyCelebrant.jsx
@@ -5,7 +5,7 @@ export default function SingleMonthlyCelebrant({ monthlyCeleb }) {
     <div className="mobile:w-5/12 w-16">
       <div className="py-1 px-2.5 bg-accent1 rounded-t-md text-primary3 font-varela font-bold text-lg flex flex-row justify-between">
         <h4>{monthlyCeleb.birth_date.day}</h4>
-        <h4>{monthlyCeleb.first_name}</h4>
+        <h4>{monthlyCeleb.first_name.toUpperCase()}</h4>
       </div>
       <img src={`${monthlyCeleb.avatar_url}`} alt="monthly celebrant avatar" />
     </div>
